588d90e173a9027ea2e86d52bbbf8857de39c3e2
Fabian Alfonso <falfonso@unbosque.edu.co>
2020-02-19 08:03:42 -0500

    Adding more functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,url_for
 from werkzeug.utils import secure_filename
 import os
 app = Flask(__name__)
@@ -9,26 +9,33 @@ def home():
     txt = ""
     error = False
     if(request.method == "POST"):
-        if(request.form['twt'].strip()):
+        if(len(request.form['twt'].strip()) > 0):
             txt = request.form['twt']
             aux = True
         else:
             error=True
     return render_template("home.html", exist = aux, txt=txt,error=error)
 
-@app.route("/file",methods=['POST','GET'])
+@app.route("/file",methods=['GET'])
+def fileget():
+    return redirect(url_for('home'))
+
+@app.route("/file",methods=['POST'])
 def file():
     aux = None
-    txt = ""
+    arr=[]
     error = False
     if(request.method == "POST"):
         f = request.files['archivo']
-        aux = True
+        
         f.save(secure_filename(f.filename))
         archivo = open(secure_filename(f.filename),"r")
         for i in archivo:
-            txt = txt + i
-    return render_template("home.html", exist = aux, txt=txt,error=error)   
+            if(i.strip()!=''):
+                arr.append(i.strip())
+        aux = True if len(arr)>0 else False
+        error = False if len(arr)>0 else True
+    return render_template("home.html", exist = aux, arr=arr, length=len(arr),error=error)   
     
 if __name__ == "__main__":
     app.run(debug=True)
